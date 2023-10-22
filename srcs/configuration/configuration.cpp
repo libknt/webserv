2e7ef3f12cbc1adddfe4487ec818ec032a1733dd
Sota Okuno <sokuno@c1r23s1.42tokyo.jp>
2023-10-22 21:20:47 +0900

    feat: スペース区切りで単語に分ける

--- srcs/configuration/configuration.cpp
+++ srcs/configuration/configuration.cpp
@@ -21,18 +21,35 @@ Configuration& Configuration::operator=(const Configuration& other) {
 	return *this;
 }
 
+std::vector<std::string> split(const std::string &s, char delim) {
+    std::vector<std::string> elems;
+    std::stringstream ss(s);
+    std::string item;
+    while (getline(ss, item, delim)) {
+    if (!item.empty()) {
+            elems.push_back(item);
+        }
+    }
+    return elems;
+}
+
+// tokenize, parseに分けても良さそうな気がしてきた
 Configuration& parseConfiguration(Configuration& configuration, std::string path) {
 	std::ifstream conf_file(path);
 	std::string line;
+	std::vector<std::string> words;
 
 	while (getline(conf_file, line)) {
-		for (size_t i = 0; i < line.size(); i++) {
-			// スペースを読み飛ばす
-			while (isspace(line[i]))
-				i++;
-			// word単位に分ける
-			// 文法が正しいか判断する(directiveによって文法が違うから持っておく必要がある)
+		// スペースを飛ばしながら単語に分ける
+		std::stringstream stringstream(line);
+		std::string word;
+		while (stringstream >> word) {
+			words.push_back(word);
 		}
+		// 文法が正しいか判断する(directiveによって文法が違うから持っておく必要がある)
+	}
+	for (size_t i = 0; i < words.size(); i++) {
+		std::cout << words[i] << std::endl;
 	}
 	return configuration;
 }
--- srcs/configuration/configuration.hpp
+++ srcs/configuration/configuration.hpp
@@ -4,6 +4,7 @@
 # include <string>
 # include <iostream>
 # include <fstream>
+# include <sstream>
 # include "location.hpp"
 
 class Configuration {
