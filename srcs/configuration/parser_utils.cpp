e1f06ee772a54fe72256e61cbcbaf0baed2b3d87
Sota Okuno <sokuno@c1r23s1.42tokyo.jp>
2023-10-26 12:25:22 +0900

    feat: extractTokensUntilSemicolonを実装

---
+++ srcs/configuration/parser_utils.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace ParserUtils {
+
+    std::vector<std::string> extractTokensUntilSemicolon(std::vector<std::string>& tokens) {
+        std::vector<std::string> extracted_tokens;
+
+        while (!tokens.empty()) {
+            std::string token = tokens.front();
+            tokens.erase(tokens.begin());
+            if (token == ";") {
+                break;
+            }
+            extracted_tokens.push_back(token);
+        }
+
+        return extracted_tokens;
+    }
+
+}
