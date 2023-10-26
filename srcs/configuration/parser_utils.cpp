41fafc9cdea6705980337afb544304e7d7723c54
Sota Okuno <sokuno@c1r23s1.42tokyo.jp>
2023-10-26 16:32:46 +0900

    feat: extractTokensUntilSemicolonにセミコロンがなかった場合のエラー処理を追加

--- srcs/configuration/parser_utils.cpp
+++ srcs/configuration/parser_utils.cpp
@@ -36,14 +36,21 @@ namespace ParserUtils {
     // セミコロンがない場合の処理
     std::vector<std::string> extractTokensUntilSemicolon(std::vector<std::string>& tokens) {
         std::vector<std::string> extracted_tokens;
+        bool has_semicolon = false;
 
         while (!tokens.empty()) {
             std::string token = tokens.front();
             tokens.erase(tokens.begin());
             if (token == ";") {
+                has_semicolon = true;
                 break;
             }
             extracted_tokens.push_back(token);
+        }
+        if (!has_semicolon) {
+            std::cerr << "Parse Error: Invalid Simple Directive" << std::endl;
+            extracted_tokens.clear();
+            return extracted_tokens;
         }
         return extracted_tokens;
     }
