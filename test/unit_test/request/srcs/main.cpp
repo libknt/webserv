3fa8c11f2f8a4621ce82b78fba6bae2f06752d9c
MasaDevs <MasaDevs@gmail.com>
2023-11-02 17:44:09 +0900

    バッファサイズの指定とクリアの指定を変えました。

--- test/unit_test/request/srcs/main.cpp
+++ test/unit_test/request/srcs/main.cpp
@@ -1,6 +1,3 @@
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 2
-#endif
 #include "http_request.hpp"
 #include "parse_http_request.hpp"
 #include <fcntl.h>
@@ -10,6 +7,10 @@
 #include <unistd.h>
 #include <vector>
 
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 12
+#endif
+
 int main(int argc, char* argv[]) {
 	server::ParseHttpRequest parse_http_request;
 	std::vector<int> fd(argc);
@@ -18,6 +19,7 @@ int main(int argc, char* argv[]) {
 	sockaddr_in server_address;
 	sockaddr_in client_address;
 
+	std::cout << "BUFFER_SIZE: " << BUFFER_SIZE << std::endl;
 	for (int i = 1; i < argc; i++) {
 		fd[i] = open(argv[i], O_RDWR);
 		parse_http_request.addAcceptClientInfo(fd[i], client_address, server_address);
