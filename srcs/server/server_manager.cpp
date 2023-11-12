046464cbe37ff47dd93d8b2be06a473151a3c26f
libknt <101494707+libknt@users.noreply.github.com>
2023-11-12 13:00:16 +0900

    runserverの移動

--- srcs/server/server_manager.cpp
+++ srcs/server/server_manager.cpp
@@ -43,6 +43,15 @@ ServerManager& ServerManager::operator=(const ServerManager& other) {
 	return *this;
 }
 
+int ServerManager::runServer() {
+	if (setupServerSockets() < 0) {
+		std::cerr << "setupServerSocket() failed" << std::endl;
+		return -1;
+	}
+	setupSelectReadFds();
+	return monitorSocketEvents();
+}
+
 int ServerManager::setupServerSockets() {
 	std::vector<ServerDirective> server_configurations = configuration_.getServers();
 	for (size_t i = 0; i < server_configurations.size(); ++i) {
@@ -365,15 +374,6 @@ int ServerManager::disconnect(int sd) {
 	close(sd);
 	std::cout << "  Connection closed - " << sd << std::endl;
 	return 0;
-}
-
-int ServerManager::runServer() {
-	if (setupServerSockets() < 0) {
-		std::cerr << "setupServerSocket() failed" << std::endl;
-		return -1;
-	}
-	setupSelectReadFds();
-	return monitorSocketEvents();
 }
 
 }
