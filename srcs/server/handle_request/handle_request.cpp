8cb046e53ce8e8b815151366ac1e63b93a455951
Sota Okuno <soooku.0806@gmail.com>
2023-11-12 22:55:48 +0900

    feat: createErrorResponseを使用

--- srcs/server/handle_request/handle_request.cpp
+++ srcs/server/handle_request/handle_request.cpp
@@ -44,22 +44,24 @@ HttpResponse executePost(const HttpRequest& request, const ServerDirective& serv
 
 HttpResponse executeDelete(const HttpRequest& request, const ServerDirective& server_directive) {
 	HttpResponse response;
-	const char* request_path = request.getRequestPath().c_str();
 	struct stat file_status;
 
-	if (stat(request_path, &file_status) == -1) {
+	if (stat(request.getRequestPath().c_str(), &file_status) == -1) {
 		std::cerr << "DELETE Error: stat() failed" << std::endl;
-		return executeError(request, server_directive);
+		response.setStatusCode(NOT_FOUND);
+		return createErrorResponse(response, server_directive);
 	}
 
 	if (!(file_status.st_mode & S_IROTH) || !(file_status.st_mode & S_IWOTH)) {
 		std::cerr << "DELETE Error: Permission denied" << std::endl;
-		return executeError(request, server_directive);
+		response.setStatusCode(BAD_REQUEST);
+		return createErrorResponse(response, server_directive);
 	}
 
 	if (remove(request.getRequestPath().c_str()) != 0) {
 		std::cerr << "DELETE Error: remove() falied" << std::endl;
-		return executeError(request, server_directive);
+		response.setStatusCode(BAD_REQUEST);
+		return createErrorResponse(response, server_directive);
 	}
 
 	return response;
