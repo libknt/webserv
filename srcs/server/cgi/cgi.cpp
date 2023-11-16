c4d21b468865aabfba442623b808ba6d378d9d9a
libknt <101494707+libknt@users.noreply.github.com>
2023-11-16 13:11:09 +0900

    feat:　一旦commit

--- srcs/server/cgi/cgi.cpp
+++ srcs/server/cgi/cgi.cpp
@@ -2,7 +2,16 @@
 
 namespace server {
 
-Cgi::~Cgi() {}
+Cgi::~Cgi() {
+	if (execve_argv != NULL) {
+		for (int i = 0; execve_argv[i] != NULL; i++) {
+			delete[] execve_argv[i];
+		}
+		delete[] execve_argv;
+	}
+	close(socket_vector_[0]);
+	close(socket_vector_[1]);
+}
 
 Cgi::Cgi(const Cgi& other)
 	: request_(other.request_)
@@ -11,6 +20,8 @@ Cgi::Cgi(const Cgi& other)
 
 	socket_vector_[0] = other.socket_vector_[0];
 	socket_vector_[1] = other.socket_vector_[1];
+
+	execve_argv = DeepCopyCharPointerArray(other.execve_argv);
 }
 
 Cgi& Cgi::operator=(const Cgi& other) {
@@ -18,6 +29,7 @@ Cgi& Cgi::operator=(const Cgi& other) {
 		meta_variables_ = other.meta_variables_;
 		socket_vector_[0] = other.socket_vector_[0];
 		socket_vector_[1] = other.socket_vector_[1];
+		execve_argv = DeepCopyCharPointerArray(other.execve_argv);
 		has_body = other.has_body;
 	}
 	return *this;
@@ -26,6 +38,7 @@ Cgi& Cgi::operator=(const Cgi& other) {
 Cgi::Cgi(const HttpRequest& request)
 	: request_(request)
 	, meta_variables_(request)
+	, execve_argv(NULL)
 	, has_body(false) {
 	socket_vector_[0] = (-1);
 	socket_vector_[1] = (-1);
@@ -36,9 +49,53 @@ int Cgi::setupInterProcessCommunication() {
 		std::cerr << "socketpair() failed(): " << strerror(errno) << std::endl;
 		return -1;
 	}
+	for(int i = 0; i < 2; i++) {
+		int flags = fcntl(socket_vector_[i], F_GETFL, 0);
+		if( flags < 0) {
+			std::cerr << "fcntl() failed(): " << strerror(errno) << std::endl;
+			return -1;
+		}
+
+		flags |= O_NONBLOCK;
+		flags = fcntl(socket_vector_[i], F_SETFL, flags);
+		if(flags < 0) {
+			std::cerr << "fcntl() failed(): " << strerror(errno) << std::endl;
+			return -1;
+		}
+	}
 	if (hasBody()) {
 		has_body = true;
 	}
+	return 0;
+}
+
+int Cgi::setupExecveArgv() {
+	execve_argv = new (std::nothrow) char*[3];
+	if (!execve_argv) {
+		return -1;
+	}
+
+	std::string path = "/usr/bin/python3";
+
+	execve_argv[0] = new (std::nothrow) char[path.size() + 1];
+	if (!execve_argv[0]) {
+		delete[] execve_argv;
+		return -1;
+	}
+	std::strcpy(execve_argv[0], path.c_str());
+
+	std::string script = meta_variables_.getMetaVariables("SCRIPT_NAME");
+	script = "/home/ubuntu2204/Documents/prg/42tokyo/webserv" + script;
+	execve_argv[1] = new (std::nothrow) char[script.size() + 1];
+	if (!execve_argv[1]) {
+		delete[] execve_argv[0];
+		delete[] execve_argv;
+		return -1;
+	}
+	std::strcpy(execve_argv[1], script.c_str());
+
+	execve_argv[2] = NULL;
+
 	return 0;
 }
 
@@ -56,7 +113,47 @@ int Cgi::setup() {
 		std::cerr << "Cgi::setupInterProcessCommunication() failed" << std::endl;
 		return -1;
 	}
-	std::cout << meta_variables_ << std::endl;
+	if (setupExecveArgv() < 0) {
+		std::cerr << "Cgi::setupExecveArgv() failed" << std::endl;
+		return -1;
+	}
+	return 0;
+}
+
+int Cgi::executeCgi() {
+	char** env = meta_variables_.getCgiEnviron();
+	pid = fork();
+	if (pid == 0) {
+		close(socket_vector_[0]);
+		if (dup2(socket_vector_[1], STDOUT_FILENO) < 0) {
+			std::exit(EXIT_FAILURE);
+		}
+		if (has_body) {
+			if (dup2(socket_vector_[1], STDIN_FILENO) < 0) {
+				std::exit(EXIT_FAILURE);
+			}
+		}
+		char** exec_argv = getExecveArgv();
+		execve("/usr/bin/python3", exec_argv, env);
+		std::exit(EXIT_FAILURE);
+	}
+	close(socket_vector_[1]);
+	// FD_SET(socket_vector_[0], &readfds);
+	return 0;
+}
+
+int Cgi::createCgiResponse() {
+	int status;
+	char buf[BUFFER_SIZE];
+	int nbytes;
+	nbytes = read(socket_vector_[0], buf, BUFFER_SIZE - 1);
+	buf[nbytes] = '\0';
+	std::cout << "buf: " << buf << std::endl;
+	if (nbytes == 0) {
+		close(socket_vector_[0]);
+		waitpid(pid, &status, 0);
+		return 1;
+	}
 	return 0;
 }
 
@@ -66,6 +163,14 @@ bool Cgi::hasBody() const {
 
 bool Cgi::getHasbody() const {
 	return has_body;
+}
+
+char** Cgi::getExecveArgv() const {
+	return execve_argv;
+}
+
+int Cgi::getSocketVector(int index) const {
+	return socket_vector_[index];
 }
 
 }
--- srcs/server/cgi/cgi.hpp
+++ srcs/server/cgi/cgi.hpp
@@ -2,9 +2,15 @@
 #define CGI_HPP
 
 #include "cgi_meta_variables.hpp"
+#include "define.hpp"
 #include "http_request.hpp"
+#include <cstdlib>
 #include <cstring>
 #include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 namespace server {
 
@@ -12,11 +18,15 @@ class Cgi {
 private:
 	Cgi();
 	int setupInterProcessCommunication();
+	int setupExecveArgv();
 
 	const HttpRequest& request_;
 	CgiMetaVariables meta_variables_;
 	int socket_vector_[2];
+	char** execve_argv;
 	bool has_body;
+	pid_t pid;
+	std::string response_body;
 
 public:
 	Cgi(const HttpRequest& request);
@@ -26,6 +36,10 @@ public:
 	int setup();
 	bool hasBody() const;
 	bool getHasbody() const;
+	char** getExecveArgv() const;
+	int executeCgi();
+	int createCgiResponse();
+	int getSocketVector(int index) const;
 };
 
 } // namespace cgi
