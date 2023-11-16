6aecf6b44c7258c9fd6d9327943751662cd864ee
MasaDevs <MasaDevs@gmail.com>
2023-11-16 19:20:27 +0900

    add

--- srcs/server/request/http_request.cpp
+++ srcs/server/request/http_request.cpp
@@ -52,28 +52,12 @@ HttpRequest& HttpRequest::operator=(HttpRequest const& other) {
 	return (*this);
 }
 
-int HttpRequest::parseHttpRequest(std::string const& line) {
-	switch (status_) {
-		case http_request_status::METHOD:
-			parseMethod(line);
-			break;
-		case http_request_status::HEADER:
-			parseHeader(line);
-			break;
-		case http_request_status::BODY:
-			parseBody(line);
-			break;
-		default:
-			setStatus(http_request_status::ERROR);
-			break;
-	}
-	if (status_ == http_request_status::ERROR)
-		return (-1);
-	return (0);
-}
-
 http_request_status::HTTP_REQUEST_STATUS const& HttpRequest::getStatus(void) const {
 	return (status_);
+}
+
+http_error_status::HTTP_ERROR_STATUS const& HttpRequest::getErrorStatus(void) const {
+	return (error_status_);
 }
 
 std::string const HttpRequest::getMethod() const {
@@ -116,14 +100,14 @@ std::string const& HttpRequest::getRequestPath() const {
 	return request_path_;
 }
 
-std::string const HttpRequest::getHeaderValue(std::string const& key) {
-	if (header_.empty())
+std::string const HttpRequest::getHeaderValue(std::string const& key) const{
+	std::map<std::string, std::string>::const_iterator it = header_.begin();
+	if (it == header_.begin())
 		return (std::string(""));
-	else
-		return (header_[key]);
+	return it->second;
 }
 
-http_body_message_type::HTTP_BODY_MESSAGE_TYPE const& HttpRequest::getBodyMessageType(void) {
+http_body_message_type::HTTP_BODY_MESSAGE_TYPE const& HttpRequest::getBodyMessageType() {
 	return (body_message_type_);
 }
 
@@ -151,123 +135,6 @@ void HttpRequest::setErrorStatus(http_error_status::HTTP_ERROR_STATUS const& err
 	error_status_ = error_status;
 }
 
-int HttpRequest::parseMethod(std::string const& line) {
-	std::vector<std::string> method_vector;
-	if (parseSentense(line, "%s %s %s", method_vector) == -1 || method_vector.size() != 3) {
-		std::cerr << "Http Method Parse Error" << std::endl;
-		setStatus(http_request_status::ERROR);
-		setErrorStatus(http_error_status::BAD_REQUEST);
-		return (-1);
-	}
-	if (setMethod(method_vector[0]) < 0 || setRequestPath(method_vector[1]) < 0 ||
-		setVersion(method_vector[2]) < 0) {
-		std::cerr << "Http Method Parse Error" << std::endl;
-		setStatus(http_request_status::ERROR);
-		setErrorStatus(http_error_status::BAD_REQUEST);
-		return (-1);
-	}
-	setStatus(http_request_status::HEADER);
-	return (0);
-}
-
-int HttpRequest::parseHeader(std::string const& line) {
-	std::vector<std::string> header_vector;
-	if (line == "\0") {
-		if (checkHeaderValue() < 0) {
-			setStatus(http_request_status::ERROR);
-			setErrorStatus(http_error_status::BAD_REQUEST);
-			return (-1);
-		}
-		if (method_ == http_method::GET || method_ == http_method::DELETE)
-			setStatus(http_request_status::FINISHED);
-		else
-			setStatus(http_request_status::BODY);
-		return (0);
-	}
-	// TODO Later: http request compromise 0 space between : and value!
-	else if (parseSentense(line, "%s: %s", header_vector) < 0 || header_vector.size() != 2) {
-		setStatus(http_request_status::ERROR);
-		setErrorStatus(http_error_status::BAD_REQUEST);
-		return (-1);
-	}
-	if (setHeaderValue(header_vector[0], header_vector[1]) < 0) {
-		setStatus(http_request_status::ERROR);
-		setErrorStatus(http_error_status::BAD_REQUEST);
-		return (-1);
-	}
-	return (0);
-}
-
-int HttpRequest::checkHeaderValue() {
-	switch (method_) {
-		case http_method::GET:
-		case http_method::DELETE:
-			body_message_type_ = http_body_message_type::NONE;
-			break;
-		case http_method::POST:
-			if (getHeaderValue("Transfer-Encoding") == "chunked")
-				body_message_type_ = http_body_message_type::CHUNK_ENCODING;
-			else if (getHeaderValue("Content-Length") != "") {
-				body_message_type_ = http_body_message_type::CONTENT_LENGTH;
-				// TODO you should check the value is affordable.
-				content_length_ =
-					static_cast<size_t>(std::atoi(getHeaderValue("Content-Length").c_str()));
-			}
-			break;
-		default:
-			setStatus(http_request_status::ERROR);
-			setErrorStatus(http_error_status::BAD_REQUEST);
-			return (-1);
-	}
-	return (0);
-}
-
-int HttpRequest::parseBody(std::string const& line) {
-	switch (body_message_type_) {
-		case http_body_message_type::CHUNK_ENCODING:
-			return (parseChunkedBody(line));
-		case http_body_message_type::CONTENT_LENGTH:
-			return (parseContentLengthBody(line));
-		default:
-			setStatus(http_request_status::ERROR);
-			setErrorStatus(http_error_status::BAD_REQUEST);
-			return (-1);
-	}
-}
-
-int HttpRequest::parseContentLengthBody(std::string const& line) {
-	// TODO  no error handling
-	body_ += line;
-	if (body_.size() == content_length_)
-		setStatus(http_request_status::FINISHED);
-	else if (content_length_ < body_.size()) {
-		setStatus(http_request_status::ERROR);
-		setErrorStatus(http_error_status::BAD_REQUEST);
-		return (-1);
-	}
-	return (0);
-}
-
-int HttpRequest::parseChunkedBody(std::string const& line) {
-	if (chunked_status_ == chunked_status::CHUNKED_SIZE) {
-		// TODO no error handling
-		// TODO Caution ! strtol is C++ 11 function.
-		chunked_size_ = std::strtol(line.c_str(), NULL, 16);
-		chunked_status_ = chunked_status::CHUNKED_MESSAGE;
-	} else if (line == "" && chunked_size_ == 0) {
-		setStatus(http_request_status::FINISHED);
-	} else {
-		if (chunked_size_ != line.size()) {
-			setStatus(http_request_status::ERROR);
-			setErrorStatus(http_error_status::BAD_REQUEST);
-			return (-1);
-		}
-		body_ += line;
-		chunked_status_ = chunked_status::CHUNKED_SIZE;
-	}
-	return (0);
-}
-
 int HttpRequest::setMethod(std::string const& method) {
 	if (method == "GET")
 		method_ = http_method::GET;
@@ -283,7 +150,7 @@ int HttpRequest::setMethod(std::string const& method) {
 }
 
 int HttpRequest::setRequestPath(std::string const& request_path) {
-	if (request_path == "\0") {
+	if (request_path.size() == 0) {
 		setStatus(http_request_status::ERROR);
 		return (-1);
 	}
@@ -306,6 +173,32 @@ int HttpRequest::setVersion(std::string const& version) {
 int HttpRequest::setHeaderValue(std::string const& key, std::string const& value) {
 	header_.insert(std::make_pair(key, value));
 	return (0);
+}
+
+int HttpRequest::setHeaderValue(std::string const& key, std::string const& value) {
+
+}
+
+int HttpRequest::setBodyMassageType(http_body_message_type::HTTP_BODY_MESSAGE_TYPE const &body_message_type) {
+
+}
+int HttpRequest::setContentLength(size_t content_length) {
+
+}
+int HttpRequest::setChunkedStatus(chunked_status::CHUNKED_STATUS const &chunked_status ) {
+
+}
+int HttpRequest::setChunkedSize(size_t chunked_size_) {
+
+}
+int HttpRequest::setBody(std::string const &body) {
+
+}
+int HttpRequest::setClientAddress(sockaddr_in const &client_address) {
+
+}
+int HttpRequest::setServerAddress(sockaddr_in const &server_address) {
+
 }
 
 std::ostream& operator<<(std::ostream& out, const HttpRequest& request) {
