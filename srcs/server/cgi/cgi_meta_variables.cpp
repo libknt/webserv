0ba2c2d0f5ec399415d4ace0274a9098ef7864b2
libknt <101494707+libknt@users.noreply.github.com>
2023-11-27 01:47:02 +0900

    feat: cgi classにHttpRequestの追加

--- srcs/server/cgi/cgi_meta_variables.cpp
+++ srcs/server/cgi/cgi_meta_variables.cpp
@@ -1,20 +1,35 @@
 #include "cgi_meta_variables.hpp"
 
-CgiMetaVariables::CgiMetaVariables()
-	: meta_variables_(std::map<std::string, std::string>()) {}
+namespace server {
+
+CgiMetaVariables::CgiMetaVariables(HttpRequest const& request)
+	: request_(request)
+	, meta_variables_(std::map<std::string, std::string>())
+	, environ_(NULL) {}
 
 CgiMetaVariables::CgiMetaVariables(CgiMetaVariables const& other)
-	: meta_variables_(other.meta_variables_) {}
+	: request_(other.request_)
+	, meta_variables_(other.meta_variables_)
+	, environ_(other.environ_) {}
 
 CgiMetaVariables& CgiMetaVariables::operator=(CgiMetaVariables const& other) {
 	if (this != &other) {
 		meta_variables_ = meta_variables_;
+		// todo deepcopy
+		environ_ = other.environ_;
 	}
 	return *this;
 }
 
-CgiMetaVariables::~CgiMetaVariables() {}
+CgiMetaVariables::~CgiMetaVariables() {
+	if (environ_) {
+		delete[] environ_;
+		delete[] environ_;
+	}
+}
 
 void CgiMetaVariables::setMetaVariables(std::string const& key, std::string const& value) {
 	meta_variables_.insert(std::make_pair(key, value));
 }
+
+} // namespace server
--- srcs/server/cgi/cgi_meta_variables.hpp
+++ srcs/server/cgi/cgi_meta_variables.hpp
@@ -1,19 +1,28 @@
 #ifndef CGI_META_VARIABLES_HPP
 #define CGI_META_VARIABLES_HPP
 
+#include "http_request.hpp"
 #include <map>
 #include <string>
 
+namespace server {
+
 class CgiMetaVariables {
 private:
+	HttpRequest const& request_;
 	std::map<std::string, std::string> meta_variables_;
+	char** environ_;
+
+	CgiMetaVariables();
 	void setMetaVariables(std::string const& key, std::string const& value);
 
 public:
-	CgiMetaVariables();
+	CgiMetaVariables(HttpRequest const& request);
 	CgiMetaVariables(CgiMetaVariables const& other);
 	CgiMetaVariables& operator=(CgiMetaVariables const& other);
 	~CgiMetaVariables();
 };
+
+} // namespace server
 
 #endif
