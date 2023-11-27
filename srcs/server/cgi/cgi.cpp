1e601460a2101a5181808427ab832c9198fadc26
libknt <101494707+libknt@users.noreply.github.com>
2023-11-27 17:31:58 +0900

    feat: setupInterProcessCommunication()

--- srcs/server/cgi/cgi.cpp
+++ srcs/server/cgi/cgi.cpp
@@ -28,4 +28,35 @@ Cgi& Cgi::operator=(const Cgi& other) {
 
 Cgi::~Cgi() {}
 
+int Cgi::setNonBlocking(int sd) {
+	int flags = fcntl(sd, F_GETFL, 0);
+	if (flags < 0) {
+		std::cerr << "fcntl() get flags failed: " << strerror(errno) << std::endl;
+		return -1;
+	}
+
+	flags |= O_NONBLOCK;
+	flags = fcntl(sd, F_SETFL, flags);
+	if (flags < 0) {
+		std::cerr << "fcntl() set flags failed: " << strerror(errno) << std::endl;
+		return -1;
+	}
+
+	return 0;
+}
+
+int Cgi::setupInterProcessCommunication() {
+	if (socketpair(AF_UNIX, SOCK_STREAM, 0, socket_vector_) == -1) {
+		std::cerr << "socketpair() failed(): " << strerror(errno) << std::endl;
+		return -1;
+	}
+	if (setNonBlocking(socket_vector_[0]) == -1) {
+		return -1;
+	}
+	if (setNonBlocking(socket_vector_[1]) == -1) {
+		return -1;
+	}
+	return 0;
+}
+
 } // namespace server
--- srcs/server/cgi/cgi.hpp
+++ srcs/server/cgi/cgi.hpp
@@ -1,6 +1,13 @@
 #ifndef CGI_HPP
 #define CGI_HPP
 
+// #include <sys/types.h>
+#include <cstring>
+#include <errno.h>
+#include <fcntl.h>
+#include <iostream>
+#include <sys/socket.h>
+
 namespace server {
 
 class Cgi {
@@ -8,12 +15,14 @@ private:
 	int socket_vector_[2];
 	int pid_;
 	int status_;
+	int setNonBlocking(int sd);
 
 public:
 	Cgi();
 	Cgi(const Cgi& other);
 	Cgi& operator=(const Cgi& other);
 	~Cgi();
+	int setupInterProcessCommunication();
 };
 
 } // namespace server
