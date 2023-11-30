dac86b46b9103f4c23c62a144d5d1f0da946912c
MasaDevs <110250805+MasaDevs@users.noreply.github.com>
2023-11-30 19:32:26 +0900

    feat: エラーの際は早期にリターンします。

--- srcs/server/handle_request/handle_request.cpp
+++ srcs/server/handle_request/handle_request.cpp
@@ -34,36 +34,37 @@ void executeGet(const HttpRequest& request,
 	std::string location_path =
 		location_directive.getRoot() + "/" + location_directive.getLocationPath();
 
-	if (stat(file_path.c_str(), &request_stat_info) == 0 &&
-		stat(location_path.c_str(), &location_stat_info) == 0) {
-		if (S_ISREG(request_stat_info.st_mode)) {
-			std::ifstream file_stream(file_path.c_str());
-			std::string body;
-			if (file_stream.is_open()) {
+	if (stat(file_path.c_str(), &request_stat_info) != 0 ||
+		stat(location_path.c_str(), &location_stat_info) != 0) {
+		return (createErrorResponse(response, http_status_code::NOT_FOUND, location_directive));
+	}
+	if (S_ISREG(request_stat_info.st_mode)) {
+		std::ifstream file_stream(file_path.c_str());
+		std::string body;
+		if (file_stream.is_open()) {
+			response.setStatusCode(http_status_code::OK);
+			std::getline(file_stream, body, static_cast<char>(EOF));
+			response.setHeaderValue("Content-Length", Utils::toString(body.size()));
+			response.setBody(body);
+			response.setStatus(http_response_status::RESPONSE_SENDING);
+			return;
+		}
+	} else if (S_ISDIR(request_stat_info.st_mode)) {
+		if (location_stat_info.st_ino == request_stat_info.st_ino) {
+			std::ifstream default_file_stream(
+				std::string(location_path + "/" + location_directive.getIndex()).c_str());
+			if (default_file_stream.is_open()) {
+				std::string body;
 				response.setStatusCode(http_status_code::OK);
-				std::getline(file_stream, body, static_cast<char>(EOF));
+				std::getline(default_file_stream, body, static_cast<char>(EOF));
 				response.setHeaderValue("Content-Length", Utils::toString(body.size()));
 				response.setBody(body);
 				response.setStatus(http_response_status::RESPONSE_SENDING);
 				return;
 			}
-		} else if (S_ISDIR(request_stat_info.st_mode)) {
-			if (location_stat_info.st_ino == request_stat_info.st_ino) {
-				std::ifstream default_file_stream(
-					std::string(location_path + "/" + location_directive.getIndex()).c_str());
-				if (default_file_stream.is_open()) {
-					std::string body;
-					response.setStatusCode(http_status_code::OK);
-					std::getline(default_file_stream, body, static_cast<char>(EOF));
-					response.setHeaderValue("Content-Length", Utils::toString(body.size()));
-					response.setBody(body);
-					response.setStatus(http_response_status::RESPONSE_SENDING);
-					return;
-				}
-			}
-			if (location_directive.getAutoindex() == "on")
-				return (makeAutoIndex(request, response, location_directive));
 		}
+		if (location_directive.getAutoindex() == "on")
+			return (makeAutoIndex(request, response, location_directive));
 	}
 	return (createErrorResponse(response, http_status_code::NOT_FOUND, location_directive));
 }
@@ -73,26 +74,27 @@ void executePost(const HttpRequest& request,
 	const LocationDirective& location_directive) {
 	struct stat file_info;
 	std::string file_path = location_directive.getRoot() + "/" + request.getRequestPath();
-	if (stat(file_path.c_str(), &file_info) == 0) {
-		if (S_ISREG(file_info.st_mode)) {
-			std::ofstream file_stream(file_path.c_str());
-			if (file_stream.is_open()) {
-				file_stream << request.getBody() << std::endl;
-				file_stream.close();
-				response.setStatusCode(http_status_code::CREATED);
-				response.setStatus(http_response_status::RESPONSE_SENDING);
-				return;
-			}
-		} else if (S_ISDIR(file_info.st_mode)) {
-			std::time_t time_val = std::time(NULL);
-			std::ofstream file_stream(std::string(file_path + Utils::toString(time_val)).c_str());
-			if (file_stream.is_open()) {
-				file_stream << request.getBody() << std::endl;
-				file_stream.close();
-				response.setStatusCode(http_status_code::CREATED);
-				response.setStatus(http_response_status::RESPONSE_SENDING);
-				return;
-			}
+	if (stat(file_path.c_str(), &file_info) != 0) {
+		return (createErrorResponse(response, http_status_code::NOT_FOUND, location_directive));
+	}
+	if (S_ISREG(file_info.st_mode)) {
+		std::ofstream file_stream(file_path.c_str());
+		if (file_stream.is_open()) {
+			file_stream << request.getBody() << std::endl;
+			file_stream.close();
+			response.setStatusCode(http_status_code::CREATED);
+			response.setStatus(http_response_status::RESPONSE_SENDING);
+			return;
+		}
+	} else if (S_ISDIR(file_info.st_mode)) {
+		std::time_t time_val = std::time(NULL);
+		std::ofstream file_stream(std::string(file_path + Utils::toString(time_val)).c_str());
+		if (file_stream.is_open()) {
+			file_stream << request.getBody() << std::endl;
+			file_stream.close();
+			response.setStatusCode(http_status_code::CREATED);
+			response.setStatus(http_response_status::RESPONSE_SENDING);
+			return;
 		}
 	}
 	return (createErrorResponse(response, http_status_code::NOT_FOUND, location_directive));
