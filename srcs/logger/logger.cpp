445587a484049ca51d304e9c869757662c664a09
libknt <101494707+libknt@users.noreply.github.com>
2023-12-13 17:30:00 +0900

    feat: file出力の設定

--- srcs/logger/logger.cpp
+++ srcs/logger/logger.cpp
@@ -1,7 +1,15 @@
 #include "logger.hpp"
 
-Logger::Logger(LogLevel level)
-	: currentLevel(level) {}
+Logger::Logger(LogLevel level, std::string const& filename) 
+	: currentLevel(level) {
+		std::cout << "Logger constructor" << std::endl;
+		std::cout << "filename: " << filename << std::endl;
+		log_file_.open(filename.c_str(), std::ios::out | std::ios::app);
+	}
+
+Logger::~Logger() {
+	log_file_.close();
+}
 
 Logger& Logger::getInstance(LogLevel level) {
 	static Logger instance(level);
@@ -10,8 +18,8 @@ Logger& Logger::getInstance(LogLevel level) {
 
 void Logger::log(LogLevel level, const std::string& message, const std::string& file, int line) {
 	if (level >= currentLevel) {
-		std::cout << std::left << std::setw(8) << LogLevelToString(level) << ":";
-		std::cout << file << ":" << line << "  " << message << std::endl;
+		log_file_ << std::left << std::setw(8) << LogLevelToString(level) << ":";
+		log_file_ << file << ":" << line << "  " << message << std::endl;
 	}
 }
 
--- srcs/logger/logger.hpp
+++ srcs/logger/logger.hpp
@@ -1,14 +1,14 @@
 #ifndef LOGGER_HPP
 #define LOGGER_HPP
 
-#define ROOT "../../"
-#define LOGFILE "log.log"
+#define LOGFILE "./log/log.log"
 #define COLOR_SWITCH "ON"
 #define LOG(level, message) Logger::getInstance().log(level, message, __FILE__, __LINE__)
 
 #include <iomanip>
 #include <iostream>
 #include <string>
+#include <fstream>
 
 enum LogLevel { DEBUG, INFO, WARNING, ERROR };
 
@@ -16,13 +16,15 @@ class Logger {
 private:
 	LogLevel currentLevel;
 	static Logger* instance;
+	std::ofstream log_file_;
 
-	Logger(LogLevel level);
+	Logger(LogLevel level, std::string const& filename=LOGFILE);
 
 	Logger(const Logger&);
 	Logger& operator=(const Logger&);
 
 public:
+	~Logger();
 	static Logger& getInstance(LogLevel level = INFO);
 
 	void log(LogLevel level, const std::string& message, const std::string& file, int line);
