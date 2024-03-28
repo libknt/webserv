13adceded023c8dc445739c0cbe5b570738db8d6
massahito <110250805+massahito@users.noreply.github.com>
2024-03-28 18:45:01 +0900

    add http_status_codeを追加。

--- includes/webserv.hpp
+++ includes/webserv.hpp
@@ -9,78 +9,51 @@
 #endif
 
 // https://tex2e.github.io/rfc-translater/html/rfc2616.html
-//    "100"  ; Section 10.1.1: Continue
-//   | "101"  ; Section 10.1.2: Switching Protocols
-//   | "200"  ; Section 10.2.1: OK
-//   | "201"  ; Section 10.2.2: Created
-//   | "202"  ; Section 10.2.3: Accepted
-//   | "203"  ; Section 10.2.4: Non-Authoritative Information
-//   | "204"  ; Section 10.2.5: No Content
-//   | "205"  ; Section 10.2.6: Reset Content
-//   | "206"  ; Section 10.2.7: Partial Content
-//   | "300"  ; Section 10.3.1: Multiple Choices
-//   | "301"  ; Section 10.3.2: Moved Permanently
-//   | "302"  ; Section 10.3.3: Found
-//   | "303"  ; Section 10.3.4: See Other
-//   | "304"  ; Section 10.3.5: Not Modified
-//   | "305"  ; Section 10.3.6: Use Proxy
-//   | "307"  ; Section 10.3.8: Temporary Redirect
-//   | "400"  ; Section 10.4.1: Bad Request
-//   | "401"  ; Section 10.4.2: Unauthorized
-//   | "402"  ; Section 10.4.3: Payment Required
-//   | "403"  ; Section 10.4.4: Forbidden
-//   | "404"  ; Section 10.4.5: Not Found
-//   | "405"  ; Section 10.4.6: Method Not Allowed
-//   | "406"  ; Section 10.4.7: Not Acceptable
-
-//   | "407"  ; Section 10.4.8: Proxy Authentication Required
-//   | "408"  ; Section 10.4.9: Request Time-out
-//   | "409"  ; Section 10.4.10: Conflict
-//   | "410"  ; Section 10.4.11: Gone
-//   | "411"  ; Section 10.4.12: Length Required
-//   | "412"  ; Section 10.4.13: Precondition Failed
-//   | "413"  ; Section 10.4.14: Request Entity Too Large
-//   | "414"  ; Section 10.4.15: Request-URI Too Large
-//   | "415"  ; Section 10.4.16: Unsupported Media Type
-//   | "416"  ; Section 10.4.17: Requested range not satisfiable
-//   | "417"  ; Section 10.4.18: Expectation Failed
-//   | "500"  ; Section 10.5.1: Internal Server Error
-//   | "501"  ; Section 10.5.2: Not Implemented
-//   | "502"  ; Section 10.5.3: Bad Gateway
-//   | "503"  ; Section 10.5.4: Service Unavailable
-//   | "504"  ; Section 10.5.5: Gateway Time-out
-//   | "505"  ; Section 10.5.6: HTTP Version not supported
-// namespace http_status_code {
-// enum STATUS_CODE {
-//	OK = 200,
-//	CREATED = 201,
-//	NO_CONTENT = 204,
-//	FOUND = 302,
-//	SEE_OTHER = 303,
-//	PERMANENT_REDIRECT = 308,
-//	BAD_REQUEST = 400,
-//	FORBIDDEN = 403,
-//	NOT_FOUND = 404,
-//	METHOD_NOT_ALLOWED = 405,
-//	INTERNAL_SERVER_ERROR = 500,
-//};
-//} // namespace http_status_code
-
 namespace http_status_code {
 enum STATUS_CODE {
+	CONTINUE = 100,
+	SWITCHING_PROTOCOLS = 101,
 	OK = 200,
 	CREATED = 201,
+	ACCEPTED = 202,
+	NON_AUTHORITATIVE_INFORMATION = 203,
 	NO_CONTENT = 204,
+	RESET_CONTENT = 205,
+	PARTIAL_CONTENT = 206,
+	MULTIPLE_CHOICES = 300,
+	MOVED_PERMANENTLY = 301,
 	FOUND = 302,
 	SEE_OTHER = 303,
+	NOT_MODIFIED = 304,
+	USE_PROXY = 305,
+	TEMPORARY_REDIRECT = 307,
 	PERMANENT_REDIRECT = 308,
 	BAD_REQUEST = 400,
+	UNAUTHORIZED = 401,
+	PAYMENT_REQUIRED = 402,
 	FORBIDDEN = 403,
 	NOT_FOUND = 404,
 	METHOD_NOT_ALLOWED = 405,
+	NOT_ACCEPTABLE = 406,
+	PROXY_AUTHENTICATION_REQUIRED = 407,
+	REQUEST_TIME_OUT = 408,
+	CONFLICT = 409,
+	GONE = 410,
+	LENGTH_REQUIRED = 411,
+	PRECONDITION_FAILED = 412,
 	REQUEST_ENTITY_TOO_LARGE = 413,
+	REQUEST_URI_TOO_LARGE = 414,
+	UNSUPPORTED_MEDIA_TYPE = 415,
+	REQUESTED_RANGE_NOT_SATISFIABLE = 416,
+	EXPECTATION_FAILED = 417,
 	INTERNAL_SERVER_ERROR = 500,
+	NOT_IMPLEMENTED = 501,
+	BAD_GATEWAY = 502,
+	SERVICE_UNAVAILABLE = 503,
+	GATEWAY_TIME_OUT = 504,
+	HTTP_VERSION_NOT_SUPPORTED = 505
 };
-} // namespace http_status_code
+}
+// namespace http_status_code
 
 #endif
